ff03ae13eb21293e4dc3f06bb7e2286fa3425754
Bxrtekm <xxwxg@proton.me>
2025-04-01 16:23:55 +0200

    fidex problems

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    path_to_file = sys.argv[2]
+    path_to_file = sys.argv[1]
     text = get_book_text(path_to_file)
     num_words = number_of_words(text)
     chars_dict = number_of_each_character(text)
